abcc17a145b7866f5f6f2ff2cb88c277ff080924
Bibin <88414576+bibin567@users.noreply.github.com>
2023-06-19 11:56:25 +0100

    file updated

--- Main.java
+++ Main.java
@@ -1,11 +1,22 @@
-package com.example.myjavaproject;
+package com.example.webserver;
 
-import static spark.Spark.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class Main {
-    public static void main(String[] args) {
-        port(8080); // Set the server port to 8080
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-        get("/", (req, res) -> "Hello, World!"); // Define a route and return  response
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/plain");
+
+        PrintWriter out = response.getWriter();
+        out.println("Hello, World!");
     }
 }
--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "default" {
-  name        = "my-webserver-sg3"
+  name        = "my-webserver-sG"
   description = "Security group for the web server"
 
   ingress {
@@ -35,40 +35,6 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.default.id]
   tags                   = {
     Name = "my-webserver"
-  }
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum install -y java-11-openjdk
-    cat <<EOF > /opt/my-java-project/Main.java
-package com.example.myjavaproject;
-
-import static spark.Spark.*;
-
-public class Main {
-    public static void main(String[] args) {
-        port(8080); // Set the server port to 8080
-
-        get("/", (req, res) -> "Hello, World!"); // Define a route and return response
-    }
-}
-EOF"
-
-    javac -d /opt/my-java-project /opt/my-java-project/Main.java
-    java -cp /opt/my-java-project com.example.myjavaproject.Main
-EOF
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/user_data.sh",
-      "/tmp/user_data.sh"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"                           # Replace with the SSH user of your EC2 instance
-      host        = self.public_ip
-    }
   }
 }
 
