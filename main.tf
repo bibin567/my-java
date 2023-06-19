9a80d516021dccf5f611498d806f3b8f4d4d0cb2
Bibin <88414576+bibin567@users.noreply.github.com>
2023-06-19 12:30:27 +0100

    file updated

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = "us-east-1"
-  version = "~> 3.0"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
 resource "aws_security_group" "default" {
